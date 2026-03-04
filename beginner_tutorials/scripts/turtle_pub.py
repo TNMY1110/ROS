53cf8797b12e05e5a1c5af3e8336e4434d081bc7
TNMY1110 <searain1110@gmail.com>
2026-03-03 23:59:23 -0800

    feat: 거북이 자동 이동 Publisher

---
+++ beginner_tutorials/scripts/turtle_pub.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+
+def turtlerun():
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('geometry_msgs')
+    rate = rospy.Rate(1)  # 1Hz = 1초에 1번
+    move = Twist()
+    
+    while not rospy.is_shutdown():
+        move.linear.x = 2.0
+        move.angular.z = 1.5
+        rospy.loginfo("발행: linear=%f, angular=%f", move.linear.x, move.angular.z)
+        pub.publish(move)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        turtlerun()
+
+    except rospy.ROSInterruptException:
+        pass
